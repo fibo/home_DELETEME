e13f619d16c0e946bb7816341e820380598d564a
fibo <fibo@users.noreply.github.com>
2015-03-18 15:34:06 +0100

    added backup

--- init.sh
+++ init.sh
@@ -1,10 +1,21 @@
 #!bin/bash
 cd
+# Backup files and folders.
+function backup_if_any () { [ -e $1 ] && mv -v $1 $1.orig; }
+backup_if_any .bash_logout
+backup_if_any .bashrc
+backup_if_any .gitignore
+backup_if_any .vimrc
+backup_if_any .aliases
+backup_if_any .software
+backup_if_any .vim
+backup_if_any .git
+# Init repo.
 git init
 git checkout -b home
 git remote add my https://github.com/fibo/home.git
 git pull my home
 git submodule update --init
 vim +PluginInstall +qall
+# Back to previous folder.
 cd -
-
