6a6b7d27d196df9774ce7829e8fd90541b68a73c
fibo <fibo@users.noreply.github.com>
2015-04-16 17:20:35 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -14,10 +14,25 @@ backup_if_any .git
 # Init repo.
 git init
 git checkout -b home
-git remote add my https://github.com/fibo/home.git
+git remote add my git@github.com:fibo/home.git
 git pull my home
 git submodule update --init
 vim +PluginInstall +qall
+# Basic git configuration prompt
+## git user.name
+GIT_USER=$(git config --global user.name)
+if [ -z "$GIT_USER" ]
+then
+    read -t 60 -p "[git config] Enter your user.name: " GIT_USER
+    git config --global user.name $GIT_USER
+fi
+## git user.email
+GIT_EMAIL=$(git config --global user.email)
+if [ -z "$GIT_EMAIL" ]
+then
+    read -t 60 -p "[git config] Enter your user.email: " GIT_EMAIL
+    git config --global user.email $GIT_EMAIL
+fi
 # Load changes into current session
 source ~/.bash_profile
 # Back to previous folder.
