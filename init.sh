6870819d46a0271e35ffef5c9d967c5ccd282dc7
fibo <fibo@users.noreply.github.com>
2016-03-04 11:50:03 -0500

    updated source

--- init.sh
+++ init.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+#
+# For more information see https://github.com/fibo/home/blob/master/README.md#installation
+
 cd
+[ -d .git ] && rm -rf .git
 BACKUP_DAY=$(date +%F)
 BACKUP_DIR=$HOME/.home_backup.$BACKUP_DAY
 mkdir -p $BACKUP_DIR
+
 function backup_if_any () {
     TARGET=$(echo $1 | tr / _)
     [ -e $1 ] && mv -v $1 $BACKUP_DIR/$TARGET.$BACKUP_DAY;
 }
 backup_if_any .gitignore
-backup_if_any .gitmodules
 curl -L https://raw.githubusercontent.com/fibo/home/home/.gitignore \
     | grep ! | cut -d ! -f2 \
         | while read x; do backup_if_any $x; done
+backup_if_any .gitmodules
 curl -L https://raw.githubusercontent.com/fibo/home/home/.gitmodules \
     | grep path | cut -d = -f2 \
         | while read x; do backup_if_any $x; done
